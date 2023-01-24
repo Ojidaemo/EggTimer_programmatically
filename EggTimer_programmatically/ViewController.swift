1434b8ca71e1146416c5e0e5eb66a367be950aa1
Ojidaemo <vit.martsinovich@gmail.com>
2023-01-24 16:13:01 +0300

    Initial Commit

---
+++ EggTimer_programmatically/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EggTimer_programmatically
+//
+//  Created by Vitali Martsinovich on 2023-01-24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
